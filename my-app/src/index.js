bf6de29a3a633df6137080bf62cc317a1da63ee5
anabulle <anabelle.piret@hotmail.com>
2022-09-08 16:44:41 +0200

    add animation

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button  className={"square " + (props.isWinning ? "square-winning" : null)}
+    onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -14,12 +15,13 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        isWinning={this.props.winningSquares.includes(i)}
+        key = {"square " + i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
-
   render() {
     return (
       <div>
@@ -71,7 +73,6 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
   }
-
   jumpTo(step) {    
     this.setState({      
       stepNumber: step,     
@@ -94,15 +95,19 @@ class Game extends React.Component {
       });
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
+      status = 'Winner: ' + winner.player;
+    } else if(winner === null){
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }else{
+      status = "la partie est terminée";
+    
     }
 
     return (
       <div className="game">
         <div className="game-board">
           <Board
+          winningSquares = {winner ? winner.line : []}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
@@ -132,11 +137,21 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+
+      return {player:squares[a], line: [a,b,c]};
     }
   }
-  return null;
+  for(let j = 0; j < squares.length ; j++){
+    if(squares[j] === null){
+      return null;
+    }
+  }
+    return;
+ 
+
+
 }
